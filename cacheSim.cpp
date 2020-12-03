1b4d24a476799c039dbcc2305be759011f260164
davidcalard <58255839+davidcalard@users.noreply.github.com>
2020-12-03 23:12:26 +0200

    Update cacheSim.cpp

--- cacheSim.cpp
+++ cacheSim.cpp
@@ -17,18 +17,22 @@ static inline unsigned extract(unsigned address,unsigned size,unsigned moshe){
 
 //-----------------------------------Way--------------------------------------//
 
-way::way(const unsigned entries_num, const unsigned block_size): entries_num(entries_num),
-tag_size(EFF_ADDRESS_SIZE - entries_num + (pow(2,block_size)/DATA_SIZE)){
-    for(unsigned i = 0; i < entries_num; i++)entry.insert(pair<unsigned ,unsigned >(i,0));
+way::way(const unsigned set_size, const unsigned block_size): entries_num(pow(2,set_size)),
+                                                              tag_size(EFF_ADDRESS_SIZE - set_size - block_size){
+    for(unsigned i = 0; i < entries_num; i++){
+        entry.insert(pair<unsigned ,unsigned >(i,0));
+        is_defined.insert(pair<unsigned,bool>(i, false));
+    }
 }
 
 void inline way::swap(unsigned address, unsigned set){
     entry.find(set)->second = extract(address,tag_size,ADDRESS_SIZE-tag_size);//tag
+    is_defined.find(set)->second = true;
 }
 
 bool way::check(unsigned address, unsigned set){
     unsigned tag = extract(address,tag_size,ADDRESS_SIZE-tag_size);
-    return tag == entry.find(set)->second;
+    return tag == entry.find(set)->second && is_defined.find(set)->second;
 }
 
 //----------------------------------level-------------------------------------//
@@ -37,10 +41,10 @@ level::level(unsigned b_s, unsigned s, unsigned a, unsigned w)
         : b_size(b_s), size(s), assoc(a), wr_alloc(w),
           hits_num(0), misses_num(0) {
     //entries calc
-    unsigned entries_num = pow(2, (size - b_size - assoc));
+    unsigned entries_num = pow(2,(size - b_size - assoc));
     //ways init
     for (int i = 0; i < pow(2,assoc); i++)
-        ways_vec.emplace_back(way(entries_num, b_size));
+        ways_vec.emplace_back(way((size - b_size - assoc), b_size));
     //LRU table init
     for (int i = 0; i < entries_num; i++) {
         LRU_table.emplace_back(vector<unsigned>());
@@ -69,8 +73,7 @@ void level::updateLRU(unsigned way_index, unsigned set) {
 }
 
 bool level::accessLevel(unsigned address,bool is_wr) {
-    unsigned set = extract(address, (size - b_size - assoc),
-                           STRAIGHTEN_BITS + (unsigned) pow(2, b_size - 5));
+    unsigned set = extract(address, (size - b_size - assoc), STRAIGHTEN_BITS + b_size);
     int way_index = findWay(address, set);
     if (way_index == UNDEFINED) {
         if (is_wr && !wr_alloc) return false;//NWA
@@ -160,53 +163,53 @@ void cache::printStats(){
 
 int main(int argc, char **argv) {
 
-	if (argc < 19) {
-		cerr << "Not enough arguments" << endl;
-		return 0;
-	}
+    if (argc < 19) {
+        cerr << "Not enough arguments" << endl;
+        return 0;
+    }
 
-	// Get input arguments
+    // Get input arguments
 
-	// File
-	// Assuming it is the first argument
-	char* fileString = argv[1];
-	ifstream file(fileString); //input file stream
-	string line;
-	if (!file || !file.good()) {
-		// File doesn't exist or some other error
-		cerr << "File not found" << endl;
-		return 0;
-	}
+    // File
+    // Assuming it is the first argument
+    char* fileString = argv[1];
+    ifstream file(fileString); //input file stream
+    string line;
+    if (!file || !file.good()) {
+        // File doesn't exist or some other error
+        cerr << "File not found" << endl;
+        return 0;
+    }
 
-	unsigned mem_cyc = 0, b_size = 0, l1_size = 0, l2_size = 0, l1_assoc = 0,
-			l2_assoc = 0, l1_cyc = 0, l2_cyc = 0, wr_alloc = 0;
+    unsigned mem_cyc = 0, b_size = 0, l1_size = 0, l2_size = 0, l1_assoc = 0,
+            l2_assoc = 0, l1_cyc = 0, l2_cyc = 0, wr_alloc = 0;
 
-	for (int i = 2; i < 19; i += 2) {
-		string s(argv[i]);
-		if (s == "--mem-cyc") {
+    for (int i = 2; i < 19; i += 2) {
+        string s(argv[i]);
+        if (s == "--mem-cyc") {
             mem_cyc = atoi(argv[i + 1]);
-		} else if (s == "--bsize") {
+        } else if (s == "--bsize") {
             b_size = atoi(argv[i + 1]);
-		} else if (s == "--l1-size") {
+        } else if (s == "--l1-size") {
             l1_size = atoi(argv[i + 1]);
-		} else if (s == "--l2-size") {
+        } else if (s == "--l2-size") {
             l2_size = atoi(argv[i + 1]);
-		} else if (s == "--l1-cyc") {
+        } else if (s == "--l1-cyc") {
             l1_cyc = atoi(argv[i + 1]);
-		} else if (s == "--l2-cyc") {
+        } else if (s == "--l2-cyc") {
             l2_cyc = atoi(argv[i + 1]);
-		} else if (s == "--l1-assoc") {
+        } else if (s == "--l1-assoc") {
             l1_assoc = atoi(argv[i + 1]);
-		} else if (s == "--l2-assoc") {
+        } else if (s == "--l2-assoc") {
             l2_assoc = atoi(argv[i + 1]);
-		} else if (s == "--wr-alloc") {
+        } else if (s == "--wr-alloc") {
             wr_alloc = atoi(argv[i + 1]);
-		} else {
-			cerr << "Error in arguments" << endl;
-			return 0;
-		}
-	}
-	unsigned init_params[9] = {mem_cyc,b_size,l1_size,l2_size,l1_assoc,l2_assoc,l1_cyc,l2_cyc,wr_alloc};
+        } else {
+            cerr << "Error in arguments" << endl;
+            return 0;
+        }
+    }
+    unsigned init_params[9] = {mem_cyc,b_size,l1_size,l2_size,l1_assoc,l2_assoc,l1_cyc,l2_cyc,wr_alloc};
     cache our_cache(init_params);
     while (getline(file, line)) {
 
@@ -233,7 +236,7 @@ int main(int argc, char **argv) {
         // DEBUG - remove this line
         cout << " (dec) " << num << endl;
 
-        operation == 'R' ? our_cache.readFromCache(num) : our_cache.writeToCache(num);
+        operation == 'r' ? our_cache.readFromCache(num) : our_cache.writeToCache(num);
 
     }
 
