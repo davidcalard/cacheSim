abe7312f0ac45959d4345f8b076f24fda79ba56d
davidcalard <58255839+davidcalard@users.noreply.github.com>
2020-12-15 16:43:05 +0200

    Update cacheSim.cpp

--- cacheSim.cpp
+++ cacheSim.cpp
@@ -22,14 +22,21 @@ way::way(const unsigned set_size, const unsigned block_size): entries_num(pow(2,
     for(unsigned i = 0; i < entries_num; i++){
         entry.insert(pair<unsigned ,unsigned >(i,0));
         is_defined.insert(pair<unsigned,bool>(i, false));
+        is_dirty.insert(pair<unsigned,bool>(i, false));
     }
 }
 
-void way::swap(unsigned address, unsigned set, pair<unsigned,bool>& removed){
-    removed.first = entry.find(set)->second;
-    removed.second = is_defined.find(set)->second;
+void way::swap(unsigned address, unsigned set, tuple<unsigned,bool,bool>& removed, bool is_write){
+    get<ADDRESS>(removed) = entry.find(set)->second;
+    get<DEFINED>(removed) = is_defined.find(set)->second;
+    get<DIRTY>(removed) = is_dirty.find(set)->second;
     entry.find(set)->second = address;
     is_defined.find(set)->second = true;
+    if(get<DEFINED>(removed))
+        is_dirty.find(set)->second = false;
+    if(is_write)
+        is_dirty.find(set)->second = true;
+
 }
 
 bool way::check(unsigned address, unsigned set){
@@ -80,14 +87,15 @@ void level::updateLRU(unsigned way_index, unsigned set, Mode mode) {
     LRU_table.at(set).emplace(iterator,way_index);
 }
 
-bool level::accessLevel(unsigned address,bool is_write, pair<unsigned,bool>& removed ){
+bool level::accessLevel(unsigned address,bool is_write, tuple<unsigned,bool,bool>& removed ){
     unsigned set = extract(address, (size - b_size - assoc), STRAIGHTEN_BITS + b_size);
     int way_index = findWay(address, set);
-    removed.second = false;
+    get<DEFINED>(removed) = false;
+    get<DIRTY>(removed) = false;
     if (way_index == UNDEFINED) {
         if (is_write && !wr_alloc) return false;//NWA
         way_index = LRU_table.at(set).begin().operator*();
-        ways_vec.at(way_index).swap(address, set, removed);
+        ways_vec.at(way_index).swap(address, set, removed, is_write);
         updateLRU(way_index, set, BACK);
         return false;
     } else {
@@ -105,11 +113,11 @@ void level::removeFromLevel(unsigned int address){
     }
 }
 
-bool level::read(unsigned address, pair<unsigned,bool>& removed){
+bool level::read(unsigned address, tuple<unsigned,bool,bool>& removed){
     return accessLevel(address, false, removed);
 }
 
-bool level::write(unsigned address, pair<unsigned,bool>& removed){
+bool level::write(unsigned address, tuple<unsigned,bool,bool>& removed){
     return accessLevel(address, true, removed);
 }
 
@@ -119,6 +127,12 @@ void inline level::incMiss(){ misses_num++;}
 
 unsigned inline level::getStat(bool mode){
     return mode ? hits_num : misses_num;
+}
+
+bool level::checkHit(unsigned int address) {
+    unsigned set = extract(address, (size - b_size - assoc), STRAIGHTEN_BITS + b_size);
+    int way_index = findWay(address, set);
+    return way_index != UNDEFINED;
 }
 
 
@@ -132,50 +146,70 @@ cache::cache(unsigned init_param[9]):
 
 void cache::writeToCache(unsigned long int address){
     requests++;
-    pair<unsigned, bool> removed;
-    if(l1.write(address, removed)){//found and replaced in l1
+    tuple<unsigned, bool, bool> removed;//0-address 1-defined 2-dirty
+    if(l1.checkHit(address)){//found in l1
+        l1.write(address, removed);//replaced in l1
         l1.incHit();
-        l2.write(address, removed);//we use inclusion so found==true for sure and is replaced without inc l2.hit, we call here so l2.LRU is updated
+        //l2.write(address, removed);//we use inclusion so found==true for sure and is replaced without inc l2.hit, we
+        // call here so l2.LRU is updated
         return;
     }
     //not found in l1
-    if(l2.wr_alloc && removed.second)
-        l2.write(removed.first,removed);//to update LRU (since there is inclusion here he is for sure already in l2)
+    if(l2.wr_alloc && get<DEFINED>(removed) && get<DIRTY>(removed))
+        l2.write(get<ADDRESS>(removed),removed);//to update LRU (since there is inclusion here he is for sure already
+        // in l2)
     l1.incMiss();
     if(l2.write(address, removed)){//found and replaced in l2
         l2.incHit();
+        l1.write(address,removed);
+        if(l2.wr_alloc && get<DEFINED>(removed) && get<DIRTY>(removed))
+            l2.write(get<ADDRESS>(removed),removed);//to update LRU (since there is inclusion here he is for sure
+            // already
+        // in l2)
         return;
     }
     //not found in cache
-    if(l2.wr_alloc && removed.second)
-        l1.removeFromLevel(removed.first);
     l2.incMiss();
+    if(l2.wr_alloc && get<DEFINED>(removed)) {
+        l1.removeFromLevel(get<ADDRESS>(removed));
+    }
+    l1.write(address,removed);
+    if(l2.wr_alloc && get<DEFINED>(removed) && get<DIRTY>(removed))
+        l2.write(get<ADDRESS>(removed),removed);//to update LRU (since there is inclusion here he is for sure already
+    // in l2)
     mem_requests++;
 }
 
 void cache::readFromCache(unsigned long int address)
 {
     requests++;
-    pair<unsigned, bool> removed;
-    if (l1.read(address,removed)) {//found in l1
+    tuple<unsigned, bool, bool> removed;//0-address 1-defined 2-dirty
+    if (l1.checkHit(address)) {//found in l1
+        l1.read(address,removed);
         l1.incHit();
-        if(removed.second){
-            l2.read(removed.first,removed);//to update LRU (since there is inclusion here he is for sure already in l2)
-        }
+        //if(removed.second){
+          //  l2.read(removed.first,removed);//to update LRU (since there is inclusion here he is for sure already in
+          //  l2)
+        //}
         return;
     }
     //miss in l1
-    if(removed.second)
-        l2.read(removed.first,removed);//to update LRU (since there is inclusion here he is for sure already in l2)
     l1.incMiss();
     if (l2.read(address,removed)) {//found in l2
         l2.incHit();
+        l1.read(address,removed);
+        if(get<DEFINED>(removed)&&get<DIRTY>(removed))
+            l2.read(get<ADDRESS>(removed),removed);//to update LRU (since there is inclusion here he is for sure already in l2)
         return;
     }
     //miss in l2
-    if(removed.second)
-        l1.removeFromLevel(removed.first);
     l2.incMiss();
+    if(get<DEFINED>(removed)) {
+        l1.removeFromLevel(get<ADDRESS>(removed));
+    }
+    l1.read(address,removed);
+    if(get<DEFINED>(removed)&&get<DIRTY>(removed))
+        l2.read(get<ADDRESS>(removed),removed);//to update LRU (since there is inclusion here he is for sure already in l2)
     mem_requests++;
 }
 
